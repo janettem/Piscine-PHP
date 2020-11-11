7e468d9d2a119e7c1ba56dc9655459607ed7ccea
Janette Metelinen <janette.metelinen@gmail.com>
2020-11-11 20:04:03 +0200

    Added negative number handling to d01/ex11

--- d01/ex11/do_op_2.php
+++ d01/ex11/do_op_2.php
@@ -8,16 +8,48 @@ function my_is_int($input)
     return (ctype_digit($input));
 }
 
-if ($argc > 1)
+function is_valid_count($array, &$n1, &$op, &$n2)
 {
-    $syntax_error = "Syntax Error\n";
-    $str = str_replace(' ', '', $argv[1]);
-    $array = preg_split("/(\+|-|\*|\/|%)/", $str, -1, PREG_SPLIT_DELIM_CAPTURE);
-    if (count($array) == 3)
+    $count = count($array);
+    if ($count == 4 && $array[0] == "-" && my_is_int($array[1]))
+    {
+        $n1 = "-".$array[1];
+        $op = $array[2];
+        $n2 = $array[3];
+        return (true);
+    }
+    else if ($count == 4 && $array[2] == "-" && my_is_int($array[3]))
+    {
+        $n1 = $array[0];
+        $op = $array[1];
+        $n2 = "-".$array[3];
+        return (true);
+    }
+    else if ($count == 5 && $array[0] == "-" && my_is_int($array[1]) &&
+    $array[3] == "-" && my_is_int($array[4]))
+    {
+        $n1 = "-".$array[1];
+        $op = $array[2];
+        $n2 = "-".$array[4];
+        return (true);
+    }
+    else if ($count == 3)
     {
         $n1 = $array[0];
         $op = $array[1];
         $n2 = $array[2];
+        return (true);
+    }
+    return (false);
+}
+
+if ($argc > 1)
+{
+    $syntax_error = "Syntax Error\n";
+    $str = str_replace(' ', '', $argv[1]);
+    $array = preg_split("/(\+|-|\*|\/|%)/", $str, -1, PREG_SPLIT_DELIM_CAPTURE | PREG_SPLIT_NO_EMPTY);
+    if (is_valid_count($array, $n1, $op, $n2))
+    {
         if (my_is_int($n1) && my_is_int($n2))
         {
             if ($op == "+")
