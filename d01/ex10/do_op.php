4c2df91be352d15caf0715336a57ec3af57b3b60
Janette Metelinen <janette.metelinen@gmail.com>
2020-11-11 18:47:18 +0200

    Added d01/ex10

---
+++ d01/ex10/do_op.php
@@ -0,0 +1,44 @@
+#!/usr/bin/php
+<?PHP
+
+function my_is_int($input)
+{
+    if ($input[0] == '-')
+        return (ctype_digit(substr($input, 1)));
+    return (ctype_digit($input));
+}
+
+if ($argc > 3)
+{
+    $n1 = str_replace(' ', '', $argv[1]);
+    $op = str_replace(' ', '', $argv[2]);
+    $n2 = str_replace(' ', '', $argv[3]);
+
+    if (my_is_int($n1) && my_is_int($n2))
+    {
+        if ($op == "+")
+            echo $n1 + $n2."\n";
+        else if ($op == "-")
+            echo $n1 - $n2."\n";
+        else if ($op == "*")
+                echo $n1 * $n2."\n";
+        else if ($op == "/")
+        {
+            if ($n2 == 0)
+                echo "0\n";
+            else
+                echo $n1 / $n2."\n";
+        }
+        else if ($op == "%")
+        {
+            if ($n2 == 0)
+                echo "0\n";
+            else
+                echo $n1 % $n2."\n";
+        }
+    }
+}
+else
+    echo "Incorrect Parameters\n";
+
+?>
