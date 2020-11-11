60ff022ab848a43b2cd6ded373326f97fd756209
Janette Metelinen <janette.metelinen@gmail.com>
2020-11-11 13:34:06 +0200

    Added d01/ex05

---
+++ d01/ex05/epur_str.php
@@ -0,0 +1,16 @@
+#!/usr/bin/php
+<?PHP
+
+if ($argc > 1)
+{
+    $array = preg_split("/ +/", $argv[1], -1, PREG_SPLIT_NO_EMPTY);
+    foreach ($array as $key=>$value)
+    {
+        if ($key > 0)
+            echo " ";
+        echo "$value";
+    }
+    echo "\n";
+}
+
+?>
