5edbabd7221cbdaeae46ed9dcc1609fbdd6fddea
Janette Metelinen <janette.metelinen@gmail.com>
2020-11-11 20:26:06 +0200

    Added d01/ex12

---
+++ d01/ex12/search_it!.php
@@ -0,0 +1,17 @@
+#!/usr/bin/php
+<?PHP
+
+foreach ($argv as $key=>$value)
+{
+    if ($key > 1)
+    {
+        $array = explode(':', $value);
+        print_r($array);
+        if (count($array) == 2 && $argv[1] == $array[0])
+            $match = $array[1];
+    }
+}
+if ($match)
+    echo "$match\n";
+
+?>
