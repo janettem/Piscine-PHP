4655cebf0f87de6ec4b32d27e229dba4a4fe9fa2
Janette Metelinen <janette.metelinen@gmail.com>
2020-11-11 20:40:44 +0200

    Removed testing from d01/ex12

--- d01/ex12/search_it!.php
+++ d01/ex12/search_it!.php
@@ -6,7 +6,6 @@ foreach ($argv as $key=>$value)
     if ($key > 1)
     {
         $array = explode(':', $value);
-        print_r($array);
         if (count($array) == 2 && $argv[1] == $array[0])
             $match = $array[1];
     }
