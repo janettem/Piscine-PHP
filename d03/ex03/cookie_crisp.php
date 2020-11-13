d36708e229b4176f496634e30bad5e81cad59e53
Janette Metelinen <janette.metelinen@gmail.com>
2020-11-13 19:24:23 +0200

    Added d03

---
+++ d03/ex03/cookie_crisp.php
@@ -0,0 +1,10 @@
+<?PHP
+
+if ($_GET["action"] === "set")
+    setcookie($_GET["name"], $_GET["value"]);
+else if ($_GET["action"] === "del")
+    setcookie($_GET["name"], "", time() - 3600);
+else if ($_GET["action"] === "get" && $_COOKIE[$_GET["name"]])
+    echo $_COOKIE[$_GET["name"]]."\n";
+
+?>
---
+++ d03/ex04/raw_text.php
@@ -0,0 +1,6 @@
+<?PHP
+
+header("Content-Type: text/plain; charset=utf-8");
+echo "<html><body>Hello</body></html>\n";
+
+?>
